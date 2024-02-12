e8dae71e7d089177c835a264ad759df82d3e743c
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-02-12 13:20:44 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1_test/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1_test._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <div class="row"> 
+            <section class="col-md-8" aria-labelledby="gettingStartedTitle">
+                <h2 id="gettingStartedTitle">ES:1</h2>
+               
+                   
+                    
+                    <br />
+                    <asp:Label ID="Label1" runat="server" Text="Clicca per visualizzare il nome e cognome"></asp:Label>
+                    <p runat="server" id="test"></p>
+                    <asp:Button ID="Button1" runat="server" Text="Click me" OnClick="Button1_Click" class="btn-outline-secondary"/>
+
+            </section>
+           
+        </div>
+    </main>
+
+</asp:Content>
---
+++ WebApplication1_test/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Web.UI;
+
+namespace WebApplication1_test
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // scrivi il codice per cambiare il colore di sfondo del bottone in rosso
+            Button1.BackColor = System.Drawing.Color.Gray;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // scrivi il codice per cambiare il testo del bottone in "Clicked" e in click me se gia cliccato
+            if (Button1.Text == "Click me")
+            {
+                Button1.Text = "Clicked";
+            }
+            else if (Button1.Text == "Clicked")
+            {
+                Button1.Text = "Click me";
+            }
+
+            //Response.Write(DateTime.Now);
+            if (Button1.Text == "Clicked")
+            {
+                Label1.Text = ConfigurationManager.AppSettings["Name"] + "  " + ConfigurationManager.AppSettings["Surname"];
+            }
+            else if (Button1.Text == "Click me")
+                Label1.Text = "Clicca per visualizzare il nome e cognome";
+
+
+
+            if (Button1.Text == "Clicked")
+            {
+                Button1.BackColor = System.Drawing.Color.LightGray;
+            }
+            else if (Button1.Text == "Click me")
+                Button1.BackColor = System.Drawing.Color.Gray;
+
+
+
+
+        }
+    }
+}
